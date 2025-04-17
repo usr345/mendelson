c8019e060e97509b33204285c2c252fd1b77c032
Ivanov Dmitriy <d.ivanov@1001tur.ru>
2025-04-17 08:37:41 +0300

    small improvements

--- Meta.v
+++ Meta.v
@@ -725,10 +725,12 @@ Proof.
       exact IH.
 Qed.
 
+(* Эта теорема будет выступать в роли индективной гипотезы для семантической полноты *)
 Lemma is_theorem_if_it_true_for_all_cases {atom : Set} `{HEqDec: EqDec atom} (f : @formula atom) (letters : list atom) (HUnique: unique letters) :
-  (forall v : atom -> bool, apply_rewriter v letters |- f) -> empty |- f.
+  (forall v : atom -> bool, apply_rewriter v letters |- f) -> theorem f.
 Proof.
   intro H.
+  unfold theorem.
   induction letters as [|x l' IH].
   - specialize H with (fun _ => true).
     simpl in H.
