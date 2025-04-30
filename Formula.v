d25e7ab4964b4fb59c236be7147a9a0267618854
Ivanov Dmitriy <d.ivanov@1001tur.ru>
2025-04-30 14:30:02 +0300

    eqb_eq proven for Formulas

--- Formula.v
+++ Formula.v
@@ -1,4 +1,5 @@
 From Mendelson Require Import FSignature.
+From Mendelson Require Import EqDec.
 
 Module Formula1 <: TFormula.
   Inductive formula {atom : Type} : Type :=
@@ -30,5 +31,85 @@ Module Formula.
     decide equality.
     now apply atom_eq.
   Qed.
+
+  Fixpoint formula_beq {atom : Set} `{EqDec atom} (A B : @formula atom) : bool :=
+    match A, B with
+    | f_atom a, f_atom b  => eqb a b
+    | f_not A', f_not B' => formula_beq A' B'
+    | f_imp A1 A2, f_imp B1 B2 => andb (formula_beq A1 B1) (formula_beq A2 B2)
+    | _, _ => false
+    end.
+
+  Lemma formula_beq_eq {atom : Set} `{EqDec atom} (A B : @formula atom) :
+    (formula_beq A B) = true <-> A = B.
+  Proof.
+    split ; intro H1.
+    - generalize dependent B.
+      induction A.
+      + intros B H1.
+        destruct B ; unfold formula_beq in H1.
+        * apply eqb_eq in H1.
+          rewrite H1.
+          reflexivity.
+        * discriminate H1.
+        * discriminate H1.
+      + intros B H1.
+        destruct B.
+        * unfold formula_beq in H1.
+          discriminate H1.
+        * simpl in H1.
+          specialize (IHA B).
+          specialize (IHA H1).
+          rewrite IHA.
+          reflexivity.
+        * simpl in H1.
+          discriminate H1.
+      + intros B H1.
+        destruct B.
+        * simpl in H1.
+          discriminate H1.
+        * simpl in H1.
+          discriminate H1.
+        * simpl in H1.
+          apply andb_prop in H1.
+          destruct H1 as [H1 H2].
+          specialize (IHA1 B1).
+          specialize (IHA1 H1).
+          specialize (IHA2 B2).
+          specialize (IHA2 H2).
+          rewrite IHA1.
+          rewrite IHA2.
+          reflexivity.
+    - generalize dependent B.
+      induction A.
+      + intros B H1.
+        rewrite <-H1.
+        simpl.
+        apply eqb_reflexive.
+      + intros B H1.
+        rewrite <-H1.
+        simpl.
+        specialize (IHA A).
+        assert (Ha : A = A).
+        { reflexivity. }
+        specialize (IHA Ha).
+        apply IHA.
+      + intros B H1.
+        rewrite <-H1.
+        simpl.
+        apply andb_true_intro.
+        split.
+        * specialize (IHA1 A1).
+        assert (Ha : A1 = A1).
+        { reflexivity. }
+        specialize (IHA1 Ha).
+        apply IHA1.
+        * specialize (IHA2 A2).
+        assert (Ha : A2 = A2).
+        { reflexivity. }
+        specialize (IHA2 Ha).
+        apply IHA2.
+  Qed.
+
 End Formula.
 Export Formula.
