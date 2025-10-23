f506bb12eb2ac6c6bbb7dc4fc5fc278ed42cda38
Ivanov Dmitriy <usr345@gmail.com>
2025-10-23 18:57:20 +0300

    1

--- solver1.hs
+++ solver1.hs
@@ -8,7 +8,7 @@ data Tree a =
   | Open_leaf (Formula a)
   | One_child (Formula a) (Tree a)
   | Sequence (Formula a) (Tree a) (Tree a)
-  | For (Formula a) (Tree a) (Tree a)
+  | Fork (Formula a) (Tree a) (Tree a)
   deriving Show
 
 has_contradiction_inner :: Eq a => [(a, Bool)] -> (a, Bool) -> Maybe(a)
@@ -34,35 +34,40 @@ has_contradiction lst =
       Just a -> Just a
 
 
-process_inner :: (Show a, Eq a) => Formula a -> [(a, Bool)] -> Maybe(Tree a) -> (Tree a, [(a, Bool)])
-process_inner f atoms subtree =
+process_inner :: (Show a, Eq a) => Formula a -> [(a, Bool)] -> Bool -> (Tree a, [(a, Bool)])
+process_inner f atoms no_calc =
   case f of
     F_atom a -> let atoms' = (a, True):atoms
                 in
-                  case subtree of
-                    Nothing -> case (has_contradiction atoms') of
+                  case no_calc of
+                    -- Нужно считать
+                    False -> case (has_contradiction atoms') of
                                  Nothing -> (Open_leaf f, atoms')
                                  Just b -> (Closed_leaf f (F_atom b), atoms')
-                    Just subtree' -> (One_child f subtree', atoms')
+                    True -> (Open_leaf f, atoms')
     F_neg (F_atom a) -> let atoms' = (a, False):atoms
                         in
-                          case subtree of
-                            Nothing -> case (has_contradiction atoms') of
+                          case no_calc of
+                            False -> case (has_contradiction atoms') of
                                          Nothing -> (Open_leaf f, atoms')
                                          Just b -> (Closed_leaf f (F_atom b), atoms')
-                            Just subtree' -> (One_child f subtree', atoms')
-    F_neg f' -> let (subtree', atoms') = process_inner f' atoms subtree
-                in
-                  (One_child f subtree', atoms')
-    F_conj f1 f2 -> let (subtree2, atoms2) = process_inner f2 atoms subtree
-                    in
-                      trace ((show subtree2) ++ " " ++ (show atoms2))
-                      $ process_inner f1 atoms2 (Just subtree2)
+                            True -> (Open_leaf f, atoms')
+    F_neg (F_neg f') -> let (subtree, atoms') = process_inner f' atoms no_calc
+                        in
+                          (One_child f subtree, atoms')
+    F_neg (F_conj f1 f2) -> let (subtree1, _) = process_inner f1 atoms no_calc
+                                (subtree2, _) = process_inner f2 atoms no_calc
+                            in
+                              (Fork f subtree1, atoms')
+    -- F_conj f1 f2 -> let (subtree2, atoms2) = process_inner f2 atoms subtree
+    --                 in
+    --                   trace ((show subtree2) ++ " " ++ (show atoms2))
+    --                   $ process_inner f1 atoms2 (Just subtree2)
     _ -> (Open_leaf f, [])
 
 process :: (Show a, Eq a) => Formula a -> Tree a
 process f =
-  let (tree, _) = process_inner f [] Nothing
+  let (tree, _) = process_inner f [] False
   in
     tree
 
@@ -77,14 +82,14 @@ atom_C = NoQuotes "C"
 f1 :: Formula NoQuotes
 f1 = F_atom atom_A
 
-f11 :: Formula NoQuotes
-f11 = F_neg (F_atom atom_A)
-
 f2 :: Formula NoQuotes
-f2 = F_neg (F_neg (F_atom atom_A))
+f2 = F_neg (F_atom atom_A)
 
 f3 :: Formula NoQuotes
-f3 = F_conj (F_atom atom_A) (F_neg (F_atom atom_A))
+f3 = F_neg (F_neg (F_atom atom_A))
+
+f4 :: Formula NoQuotes
+f4 = F_conj (F_atom atom_A) (F_neg (F_atom atom_A))
 
 
     -- F_neg (F_atom a) ->
