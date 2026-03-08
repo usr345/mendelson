e3f07bc806156e16db5a064d00be835525a011dd
Ivanov Dmitriy <dima@danilovplaza.org>
2026-03-08 12:55:55 +0300

    logic B exercises done

--- Relevant_B/Semantic.v
+++ Relevant_B/Semantic.v
@@ -336,4 +336,441 @@ Module Semantic.
     - specialize (contrapos_valid [A] A0 B IHentails) as Hvalid.
       exact Hvalid.
   Qed.
+
+  Variant atom3 : Type := P | Q | S.
+  Definition f (a: atom3) : @formula atom3 :=
+    f_atom a.
+
+  Coercion f: atom3 >-> formula.
+  Variant worlds4 : Type := Γ | Δ | Ε | Ω.
+
+  Definition is_normal1 (w: worlds4) : bool :=
+  match w with  
+  | Γ => true
+  | _ => false
+  end.
+
+  Definition R1 (w1 w2 w3: worlds4) : Prop :=
+    (w1 = Γ /\ w2 = w3) \/ (w1 = Δ /\ w2 = Ε /\ w3 = Ω).
+
+  Definition star1 (w: worlds4) : worlds4 := w.
+  Lemma Star1Involutive : forall w : worlds4, star1 (star1 w) = w.
+  Proof.
+    intro w.
+    destruct w ; unfold star1 ; reflexivity.
+  Qed.
+
+  Lemma A1_neg : ~ forall (atom : Type) (A B : @formula atom), |= $A -> (B -> A)$.
+  Proof.
+    unfold not.
+    intro H.
+    specialize (H atom3 P Q).
+
+    pose (
+      v1 :=
+        fun (a : atom3) (w: worlds4) =>
+          match a, w with
+          | P, Δ => True
+          | Q, Ε => True
+          | _, _ => False
+          end
+    ).
+
+    pose (M1 := {|
+      worlds := worlds4;
+      w0 := Γ;
+      is_normal := is_normal1;
+      R := R1;
+      star := star1;
+      star_involutive := Star1Involutive;
+      v := v1;
+    |}).
+
+    unfold valid in H.
+    specialize (H M1 M1.(w0)).
+    unfold M1 in H.
+    cbn [is_normal] in H.
+    cbn [w0] in H.
+    cbn [is_normal1] in H.
+    specialize (H eq_refl).
+    simpl in H.
+    specialize (H Δ).
+    simpl in H.
+    specialize (H I).
+    specialize (H Ε Ω).
+    simpl in H.
+    apply H.
+    - unfold R1.
+      right.
+      exact (conj eq_refl (conj eq_refl eq_refl)).
+    - exact I.
+  Qed.
+
+ Lemma T2_1_neg : ~ forall (atom : Type) (A B C : @formula atom), [$(A /\ B) -> C$] |= $A -> (B -> C)$.
+ Proof.
+  unfold not.
+  intro H.
+  specialize (H atom3 P Q S).
+
+  pose (
+    v1 :=
+      fun (a : atom3) (w: worlds4) =>
+        match a, w with
+        | P, Δ => True
+        | Q, Ε => True
+        | S, Ε => True
+        | _, _ => False
+        end
+  ).
+
+  pose (M1 := {|
+    worlds := worlds4;
+    w0 := Γ;
+    is_normal := is_normal1;
+    R := R1;
+    star := star1;
+    star_involutive := Star1Involutive;
+    v := v1;
+  |}).
+
+  unfold consequence in H.
+  specialize (H M1 M1.(w0)).
+  unfold M1 in H.
+  cbn [is_normal] in H.
+  cbn [w0] in H.
+  cbn [is_normal1] in H.
+  specialize (H eq_refl).
+  assert (Hall : holds_all
+      {|
+        worlds := worlds4;
+        w0 := Γ;
+        is_normal := is_normal1;
+        R := R1;
+        star := star1;
+        star_involutive := Star1Involutive;
+        v := v1
+      |} [$P /\ Q -> S$] Γ).
+  {
+    unfold holds_all.
+    intros f Hin.
+    unfold In in Hin.
+    destruct Hin as [Hin | []].
+    rewrite <-Hin.
+    simpl.
+    intro w.
+    destruct w ; intro H1.
+    - destruct H1 as [H1 _].
+      exact H1.
+    - destruct H1 as [_ H1].
+      exact H1.
+    - destruct H1 as [_ H1].
+      exact H1.
+    - destruct H1 as [H1 _].
+      exact H1.
+  }
+
+  specialize (H Hall).
+  simpl in H.
+  specialize (H Δ).
+  simpl in H.
+  specialize (H I).
+  specialize (H Ε Ω).
+  simpl in H.
+  apply H.
+  - unfold R1.
+    right.
+    exact (conj eq_refl (conj eq_refl eq_refl)).
+  - exact I.
+Qed.
+
+Lemma T2_3_neg : ~ forall (atom : Type) (A B C : @formula atom), |= $((A -> B) /\ (B -> C)) -> (A -> C)$.
+ Proof.
+  unfold not.
+  intro H.
+  specialize (H atom3 P Q S).
+
+  pose (
+    v1 :=
+      fun (a : atom3) (w: worlds4) =>
+        match a, w with
+        | P, Ε => True
+        | Q, Ω => True
+        | _, _ => False
+        end
+  ).
+
+  pose (M1 := {|
+    worlds := worlds4;
+    w0 := Γ;
+    is_normal := is_normal1;
+    R := R1;
+    star := star1;
+    star_involutive := Star1Involutive;
+    v := v1;
+  |}).
+
+  unfold valid in H.
+  specialize (H M1 M1.(w0)).
+  unfold M1 in H.
+  cbn [is_normal] in H.
+  cbn [w0] in H.
+  cbn [is_normal1] in H.
+  specialize (H eq_refl).
+  simpl in H.
+  specialize (H Δ).
+  cbn [is_normal1] in H.
+
+  assert (Hante : (forall x y : worlds4,
+     R1 Δ x y ->
+     match x with
+     | Ε => True
+     | _ => False
+     end ->
+     match y with
+     | Ω => True
+     | _ => False
+     end) /\
+    (forall x y : worlds4,
+     R1 Δ x y ->
+     match x with
+     | Ω => True
+     | _ => False
+     end -> False)).
+  {
+    split.
+    - intros x y HR.
+      destruct x, y ; unfold R1 in HR ; intro H1 ; try destruct H1 ; try apply I.
+      + destruct HR as [HR | HR].
+        * destruct HR as [HR _].
+          discriminate HR.
+        * destruct HR as [_ [_ HR]].
+          discriminate HR.
+      + destruct HR as [HR | HR].
+        * destruct HR as [HR _].
+          discriminate HR.
+        * destruct HR as [_ [_ HR]].
+          discriminate HR.
+      + destruct HR as [HR | HR].
+        * destruct HR as [HR _].
+          discriminate HR.
+        * destruct HR as [_ [_ HR]].
+          discriminate HR.
+    - intros x y HR.
+      destruct x, y ; unfold R1 in HR ; intro H1 ; try destruct H1 ; try apply I.
+      + destruct HR as [HR | HR].
+        * destruct HR as [HR _].
+          discriminate HR.
+        * destruct HR as [_ [_ HR]].
+          discriminate HR.
+      + destruct HR as [HR | HR].
+        * destruct HR as [HR _].
+          discriminate HR.
+        * destruct HR as [_ [_ HR]].
+          discriminate HR.
+      + destruct HR as [HR | HR].
+        * destruct HR as [HR _].
+          discriminate HR.
+        * destruct HR as [_ [_ HR]].
+          discriminate HR.
+      + destruct HR as [HR | HR].
+        * destruct HR as [HR _].
+          discriminate HR.
+        * destruct HR as [_ [HR _]].
+          discriminate HR.
+  }
+
+  specialize (H Hante).
+  clear Hante.
+  specialize (H Ε Ω).
+  simpl in H.
+  apply H.
+  - unfold R1.
+    right.
+    exact (conj eq_refl (conj eq_refl eq_refl)).
+  - exact I.
+Qed.
+
+ Lemma T2_4_neg : ~ forall (atom : Type) (A B C : @formula atom), |= $(A -> B) -> ((A /\ C)-> (B /\ C))$.
+ Proof.
+  unfold not.
+  intro H.
+  specialize (H atom3 P Q S).
+
+  pose (
+    v1 :=
+      fun (a : atom3) (w: worlds4) =>
+        match a, w with
+        | P, Ε => True
+        | S, Ε => True
+        | Q, Ω => True
+        | _, _ => False
+        end
+  ).
+
+  pose (M1 := {|
+    worlds := worlds4;
+    w0 := Γ;
+    is_normal := is_normal1;
+    R := R1;
+    star := star1;
+    star_involutive := Star1Involutive;
+    v := v1;
+  |}).
+
+  unfold valid in H.
+  specialize (H M1 M1.(w0)).
+  unfold M1 in H.
+  cbn [is_normal] in H.
+  cbn [w0] in H.
+  cbn [is_normal1] in H.
+  specialize (H eq_refl).
+  simpl in H.
+  specialize (H Δ).
+  cbn [is_normal1] in H.
+
+  assert (Hante : (forall x y : worlds4,
+     R1 Δ x y ->
+     match x with
+     | Ε => True
+     | _ => False
+     end ->
+     match y with
+     | Ω => True
+     | _ => False
+     end)).
+  {
+    intros x y HR.
+    destruct x, y ; unfold R1 in HR ; intro H1 ; try destruct H1 ; try apply I.
+    + destruct HR as [HR | HR].
+      * destruct HR as [HR _].
+        discriminate HR.
+      * destruct HR as [_ [_ HR]].
+        discriminate HR.
+    + destruct HR as [HR | HR].
+      * destruct HR as [HR _].
+        discriminate HR.
+      * destruct HR as [_ [_ HR]].
+        discriminate HR.
+    + destruct HR as [HR | HR].
+      * destruct HR as [HR _].
+        discriminate HR.
+      * destruct HR as [_ [_ HR]].
+        discriminate HR.
+  }
+
+  specialize (H Hante).
+  clear Hante.
+  specialize (H Ε Ω).
+  simpl in H.
+  assert (HR1 : R1 Δ Ε Ω).
+  {
+    unfold R1.
+    right.
+    exact (conj eq_refl (conj eq_refl eq_refl)).
+  }
+
+  specialize (H HR1).
+  specialize (H (conj I I)).
+  destruct H as [_ H].
+  exact H.
+Qed.
+
+Lemma T2_5_neg : ~ forall (atom : Type) (A B C : @formula atom), [$(A /\ B) -> C$] |= $(A /\ ~C) -> ~B$.
+ Proof.
+  unfold not.
+  intro H.
+  specialize (H atom3 P Q S).
+
+  pose (
+      R1 :=
+        fun (w1 w2 w3: worlds4) => (w1 = Γ /\ w2 = w3)
+    ).
+
+  pose (
+      star1 :=
+        fun (w: worlds4) => 
+        match w with
+        | Γ => Γ
+        | Δ => Ε
+        | Ε => Δ
+        | Ω => Ω
+        end
+    ).
+
+  assert (Hinvolutive : forall w : worlds4, star1 (star1 w) = w).
+  {
+    intro w.
+    destruct w ; unfold star1 ; reflexivity.
+  }
+
+  pose (
+    v1 :=
+      fun (a : atom3) (w: worlds4) =>
+        match a, w with
+        | P, Δ => True
+        | Q, Ε => True
+        | _, _ => False
+        end
+  ).
+
+  pose (M1 := {|
+    worlds := worlds4;
+    w0 := Γ;
+    is_normal := is_normal1;
+    R := R1;
+    star := star1;
+    star_involutive := Hinvolutive;
+    v := v1;
+  |}).
+
+  unfold consequence in H.
+  specialize (H M1 M1.(w0)).
+  unfold M1 in H.
+  cbn [is_normal] in H.
+  cbn [w0] in H.
+  cbn [is_normal1] in H.
+  specialize (H eq_refl).
+
+  assert (Hall : holds_all
+      {|
+        worlds := worlds4;
+        w0 := Γ;
+        is_normal := is_normal1;
+        R := R1;
+        star := star1;
+        star_involutive := Hinvolutive;
+        v := v1
+      |} [$P /\ Q -> S$] Γ).
+  {
+    unfold holds_all.
+    intros f Hin.
+    unfold In in Hin.
+    destruct Hin as [Hin | []].
+    rewrite <-Hin.
+    simpl.
+    intro w.
+    destruct w ; intro H1.
+    - destruct H1 as [H1 _].
+      exact H1.
+    - destruct H1 as [_ H1].
+      exact H1.
+    - destruct H1 as [H1 _].
+      exact H1.
+    - destruct H1 as [H1 _].
+      exact H1.
+  }
+
+  specialize (H Hall).
+  clear Hall.
+  simpl in H.
+  specialize (H Δ).
+  simpl in H.
+  unfold not in H.
+  apply H.
+  split.
+  - exact I.
+  - intro H1.
+    exact H1.
+  - exact I.
+Qed.
 End Semantic.
